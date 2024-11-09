31bd6d261ec241c69af069e116e0b0c7579c1123
jasmincervantes490 <jasmin.cervantes490@campus.mccd.edu>
2024-11-09 05:51:24 +0000

    Second commit, added the second method, third methods, and ran the tests for them

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@
 //Recursion and JUnit Assignment
 
 public class Main {
+    //Problem #1
     public static int count8(int n) {
         if (n == 0) {
             return 0;
@@ -19,4 +20,36 @@ public class Main {
 
         return count8(n / 10);
     }
+
+    //Problem #2
+    public static int countHi(String str) {
+        if (str.length() < 2) {
+            return 0;
+        }
+
+        if (str.substring(0, 2).equals("hi")) {
+            return 1 + countHi(str.substring(1));
+        } else {
+            return countHi(str.substring(1));
+        }
+    }
+
+    //Problem #3
+    public static int countHi2(String str) {
+        if (str.length() < 2) {
+            return 0;
+        }
+
+        if (str.startsWith("hi")) {
+            return 1 + countHi2(str.substring(2));
+        }
+
+        if (str.length() > 2 && str.charAt(0) == 'x' && str.substring(1, 3).equals("hi")) {
+            return countHi2(str.substring(3));
+        }
+
+        return countHi2(str.substring(1));
+    }
+
+    
 }
--- RecursiveTest.java
+++ RecursiveTest.java
@@ -14,16 +14,66 @@ public class RecursiveTest {
 
     @Test
     public void test3() {
-        assertEquals(Main.count8(818), 3);
+        assertEquals(Main.count8(8818), 4);
     }
 
     @Test
     public void test4() {
-        assertEquals(Main.count8(8818), 4);
+        assertEquals(Main.count8(88188), 6);
     }
     
     @Test
     public void test5() {
-        assertEquals(Main.count8(88188), 6);
+        assertEquals(Main.count8(818), 3);
+    }
+
+    @Test
+    public void test6() {
+        assertEquals(Main.countHi("xxhixx"), 1);
+    }
+
+    @Test
+    public void test7() {
+        assertEquals(Main.countHi("xhixhix"), 2);
+    }
+
+    @Test
+    public void test8() {
+        assertEquals(Main.countHi("hi"), 1);
+    }
+
+    @Test
+    public void test9() {
+        assertEquals(Main.countHi("bhi"), 1);
+    }
+
+    @Test
+    public void test10() {
+        assertEquals(Main.countHi("hicih"), 2);
+    }
+
+    @Test
+    public void test11() {
+        assertEquals(Main.countHi2("ahixhi"), 1);
+    }
+
+    @Test
+    public void test12() {
+        assertEquals(Main.countHi2("ahibhi"), 2);
+    }
+
+    @Test
+    public void test13() {
+        assertEquals(Main.countHi2("hixhi"), 1);
+    }
+
+    @Test
+    public void test14() {
+        assertEquals(Main.countHi2("xhixhi"), 0);
+    }
+
+    @Test
+    public void test15() {
+        assertEquals(Main.countHi2("xhixhi"), 2);
     }
 }
